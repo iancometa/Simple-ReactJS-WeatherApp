dab283b7b41e5ad1c837837269a2a2483d4971df
Christian <iancometa@gmail.com>
2016-06-26 00:40:16 +1000

    modified app-server.js to include bootstrap

--- app-server.js
+++ app-server.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express();
 
 app.use(express.static('./public'));
-app.use(express.static('./node-modules'));
+app.use(express.static('./node_modules/bootstrap/dist'));
+app.use(express.static('./node_modules/react/dist'));
+app.use(express.static('./node_modules/react-dom/dist'));
 
 app.listen(3000);
 console.log('server running at http://localhost:3000');
