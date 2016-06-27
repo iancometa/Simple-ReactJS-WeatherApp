7aaba8e8202ab879628aa19233dd8380eb61506e
Christian <iancometa@gmail.com>
2016-06-28 00:28:43 +1000

    modified app-server.js

--- app-server.js
+++ app-server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('./public'));
-app.use(express.static('./node_modules/bootstrap/dist'));
+app.use(express.static('./node_modules/bootstrap/dist/css'));
+app.use(express.static('./node_modules/jquery/dist'));
 app.use(express.static('./node_modules/react/dist'));
 app.use(express.static('./node_modules/react-dom/dist'));
 
